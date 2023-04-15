388f7f8249e09d156d6e5158f7562cd01039b1ce
fillipe <fillipe.trybe@gmail.com>
2023-04-15 19:03:17 -0300

    Requisito 1

---
+++ src/Controllers/CarController.ts
@@ -0,0 +1,34 @@
+import { NextFunction, Request, Response } from 'express';
+import CarService from '../Services/CarService';
+import ICar from '../Interfaces/ICar';
+
+class CarController {
+  private req: Request;
+  private res: Response;
+  private next: NextFunction;
+  private service: CarService;
+
+  constructor(req: Request, res: Response, next: NextFunction) {
+    this.req = req;
+    this.res = res;
+    this.next = next;
+    this.service = new CarService();
+  }
+
+  public async createNewCar() {
+    const car: ICar = {
+      model: this.req.body.model,
+      year: this.req.body.year,
+      color: this.req.body.color,
+      status: this.req.body.status || false,
+      buyValue: this.req.body.buyValue,
+      doorsQty: this.req.body.doorsQty,
+      seatsQty: this.req.body.seatsQty,
+    };
+
+    const newCar = await this.service.createCar(car);
+    return this.res.status(201).json(newCar);
+  }
+}
+
+export default CarController;

8b78a5b74ca82cf9e3c5dd207f9eb7264a22f730
fillipe <fillipe.trybe@gmail.com>
2023-04-17 15:08:40 -0300

    Implementando testes

---
+++ tests/unit/Services/car.test.ts
@@ -0,0 +1,71 @@
+import { Model } from 'mongoose';
+import sinon from 'sinon';
+import { expect } from 'chai';
+import carMocks from '../../mocks/carMocks';
+import CarService from '../../../src/Services/CarService';
+import Car from '../../../src/Domains/Car';
+import ICar from '../../../src/Interfaces/ICar';
+
+const carInput: ICar = {
+  model: 'Opala',
+  year: 2000,
+  color: 'red',
+  status: true,
+  buyValue: 6.000,
+  doorsQty: 2,
+  seatsQty: 5,
+  id: '642b0df19506445d7b0bc77b',
+};
+
+const carOutput: Car = new Car({
+  model: 'Opala',
+  year: 2000,
+  color: 'red',
+  status: true,
+  buyValue: 6.000,
+  doorsQty: 2,
+  seatsQty: 5,
+});
+
+describe('Testando endpoint /cars', function () {
+  it('Deveria criar um novo carro com sucesso', async function () {
+    sinon.stub(Model, 'create').resolves(carOutput);
+
+    const service = new CarService();
+    const result = await service.createCar(carInput);
+
+    expect(result).to.be.deep.equal(carOutput);
+  });
+
+  it('Deveria listar todos os carros', async function () {
+    const carsOutput = carMocks.map((car) => new Car(car));
+    sinon.stub(Model, 'find').resolves(carsOutput);
+
+    const service = new CarService();
+    const result = await service.findCars();
+
+    expect(result).to.be.deep.equal(carsOutput);
+  });
+
+  it('Deveria listar um carro encontrado pelo id', async function () {
+    sinon.stub(Model, 'findOne').resolves(carMocks[0]);
+    const id = '634852326b35b59438fbea2f';
+
+    const service = new CarService();
+    const { message } = await service.findOneCar(id);
+
+    expect(message).to.be.deep.equal(carMocks[0]);
+  });
+
+  it('Deveria lançar uma exceção quando a id não existe', async function () {
+    const carId = '000000';
+
+    sinon.stub(Model, 'findOne').resolves(false);
+
+    const service = new CarService();
+    const { message } = await service.findOneCar(carId);
+    expect(message).to.be.equal('Car not found');
+  });
+
+  afterEach(function () { return sinon.restore(); });
+});
---
+++ tests/unit/Services/motorcycle.test.ts
@@ -0,0 +1,71 @@
+import { Model } from 'mongoose';
+import sinon from 'sinon';
+import { expect } from 'chai';
+import MotorcycleService from '../../../src/Services/MotorcycleService';
+import Motorcycle from '../../../src/Domains/Motorcycle';
+import motorcyleMocks from '../../mocks/motorcycleMocks';
+import IMotorcycle from '../../../src/Interfaces/IMotorcycle';
+
+const motorcycleOutput: Motorcycle = new Motorcycle({
+  model: 'Honda Cb 600f Hornet',
+  year: 2005,
+  color: 'Yellow',
+  status: true,
+  buyValue: 30.000,
+  id: '642b1159be4b3a0428baf084',
+  category: 'Street',
+  engineCapacity: 600,
+});
+
+const motorcycleInput: IMotorcycle = {
+  model: 'Honda Cb 600f Hornet',
+  year: 2005,
+  color: 'Yellow',
+  status: true,
+  buyValue: 30.000,
+  category: 'Street',
+  engineCapacity: 600,
+};
+
+describe('Testando endpoint /motorcyles', function () {
+  it('Deveria criar um novo motorcycle com sucesso', async function () {
+    sinon.stub(Model, 'create').resolves(motorcycleOutput);
+
+    const service = new MotorcycleService();
+    const result = await service.createMotorcycle(motorcycleInput);
+
+    expect(result).to.be.deep.equal(motorcycleOutput);
+  });
+
+  it('Deveria listar todos os motorcyles', async function () {
+    const motorcyclesOutput = motorcyleMocks.map((motorcyles) => new Motorcycle(motorcyles));
+    sinon.stub(Model, 'find').resolves(motorcyclesOutput);
+
+    const service = new MotorcycleService();
+    const result = await service.findMotorcycles();
+
+    expect(result).to.be.deep.equal(motorcyclesOutput);
+  });
+
+  it('Deveria listar um motorcycle encontrado pelo id', async function () {
+    sinon.stub(Model, 'findOne').resolves(motorcyleMocks[0]);
+    const id = '634852326b35b59438fbea2f';
+
+    const service = new MotorcycleService();
+    const { message } = await service.findOneMotorcycle(id);
+
+    expect(message).to.be.deep.equal(motorcyleMocks[0]);
+  });
+
+  it('Deveria lançar uma exceção quando a id não existe', async function () {
+    const MotorcycleId = '000000';
+
+    sinon.stub(Model, 'findOne').resolves(false);
+
+    const service = new MotorcycleService();
+    const { message } = await service.findOneMotorcycle(MotorcycleId);
+    expect(message).to.be.equal('Motorcycle not found');
+  });
+
+  afterEach(function () { return sinon.restore(); });
+});
